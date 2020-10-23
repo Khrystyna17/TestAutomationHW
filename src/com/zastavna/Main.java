b01adc496acb4c325e35fcbc015f34e307253f46
Khrystyna17 <khrystynazast@gmail.com>
2020-10-23 12:55:55 +0300

    Add task 1 about flower bed

--- src/com/zastavna/Main.java
+++ src/com/zastavna/Main.java
@@ -1,8 +1,19 @@
 package com.zastavna;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the radius: ");
+        Double radius = Double.parseDouble(br.readLine());
+        Double perimeter = 2.0 * Math.PI * radius;
+        Double area = Math.PI * radius * radius;
+        System.out.println("The perimeter of the flower bed is " + perimeter);
+        System.out.println("The area of the flower bed is " + area);
     }
 }
+
