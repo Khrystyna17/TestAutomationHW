af0a95f57dbbce95779ac29089f4a9f0af081acf
Khrystyna17 <khrystynazast@gmail.com>
2020-10-23 13:04:18 +0300

    Add task 2 about name and address

--- src/com/zastavna/Main.java
+++ src/com/zastavna/Main.java
@@ -1,8 +1,20 @@
 package com.zastavna;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("What is your name?");
+        String name = br.readLine();
+        System.out.println("Where are you live, " + name + "? ");
+        String address = br.readLine();
+        System.out.println("Name: " + name);
+        System.out.println("Address: " + address);
     }
 }
+
+
